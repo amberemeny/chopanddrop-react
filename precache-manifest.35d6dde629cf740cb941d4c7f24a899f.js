3a26daa6321cf64ea2727d01b0136cb3f4d64eff
Amber Emeny <amberemeny@hotmail.com>
2020-01-07 21:54:25 +1000

    Updates

---
+++ precache-manifest.35d6dde629cf740cb941d4c7f24a899f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36be6ead2af8ed59ac2ea6440710b662",
+    "url": "/chopanddrop-react/index.html"
+  },
+  {
+    "revision": "e3e8201f486663e4aee9",
+    "url": "/chopanddrop-react/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "350279feb97bd8a61f1c",
+    "url": "/chopanddrop-react/static/css/main.1ebcc772.chunk.css"
+  },
+  {
+    "revision": "e3e8201f486663e4aee9",
+    "url": "/chopanddrop-react/static/js/2.11e351fe.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/chopanddrop-react/static/js/2.11e351fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "350279feb97bd8a61f1c",
+    "url": "/chopanddrop-react/static/js/main.b174a19b.chunk.js"
+  },
+  {
+    "revision": "c37ebb73209469c6e06f",
+    "url": "/chopanddrop-react/static/js/runtime-main.0519aeb3.js"
+  }
+]);
